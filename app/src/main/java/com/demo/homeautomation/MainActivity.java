3ed97e9348d6063b589fc35181548eda93658b5c
vj <vij.vitcs@gmail.com>
2017-11-07 19:21:16 +0530

    listener changed

--- app/src/main/java/com/demo/homeautomation/MainActivity.java
+++ app/src/main/java/com/demo/homeautomation/MainActivity.java
@@ -3,6 +3,8 @@ package com.demo.homeautomation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.Guideline;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -20,12 +22,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import ai.api.android.AIConfiguration;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements ai.api.AIListener{
     private static final String TAG = "Main activity";
     FirebaseAuth firebaseAuth;
 
@@ -33,11 +37,16 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference l1Ref;
     DatabaseReference l2Ref;
     DatabaseReference f1Ref;
+    DatabaseReference camRef;
+    DatabaseReference camLogRef;
 
     ValueEventListener l1EventListener;
     ValueEventListener l2EventListener;
     ValueEventListener f1EventListener;
+    ValueEventListener camEventListener;
 
+    @BindView(R.id.coordinator)
+    CoordinatorLayout coordinatorLayout;
     @BindView(R.id.modeSelectionGroup)
     RadioGroup modeSelectionGroup;
     @BindView(R.id.l1switch)
@@ -67,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
         l1Ref = firebaseDatabase.getReference("light1");
         l2Ref = firebaseDatabase.getReference("light2");
         f1Ref = firebaseDatabase.getReference("fan1");
+        camRef = firebaseDatabase.getReference("cam");
+        camLogRef = firebaseDatabase.getReference("camLogs");
         if (firebaseAuth.getCurrentUser() == null) {
             openLoginActivity();
         }
@@ -87,16 +98,75 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        final AIConfiguration config = new AIConfiguration("CLIENT_ACCESS_TOKEN",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
     }
 
     @Override
     protected void onResume() {
-//        startListeners();
+        startListeners();
+        startDoorListener();
         super.onResume();
     }
 
     private void fan1Change(int progress) {
         f1Ref.setValue(progress);
+    }
+
+    private void startDoorListener() {
+        removeDoorListener();
+        camEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Boolean value = dataSnapshot.getValue(Boolean.class);
+                if (value != null) {
+                    if (value) {
+                        camLogRef.limitToLast(1).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                int timestamp = dataSnapshot.child("timestamp").getValue(Integer.class);
+                                String url = dataSnapshot.child("url").getValue(String.class);
+                                camLogRef.removeEventListener(this);
+                                removeDoorListener();
+                                openDoorActivity(timestamp,url);
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+
+                        Snackbar snackbar = Snackbar
+                                .make(coordinatorLayout, "Somebody cam to your door", Snackbar.LENGTH_LONG)
+                                .setAction("View", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        removeDoorListener();
+                                    }
+                                });
+                        snackbar.show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        camRef.addValueEventListener(camEventListener);
+    }
+
+    private void openDoorActivity(int timestamp, String url) {
+
+    }
+
+    private void removeDoorListener() {
+        if (camEventListener != null) {
+            camRef.removeEventListener(camEventListener);
+        }
     }
 
     private void startListeners() {
@@ -117,8 +187,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
-        l1Ref.addValueEventListener(l1EventListener);
+        l2EventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Boolean value = dataSnapshot.getValue(Boolean.class);
+                if (value != null) {
+                    if (value) {
+                        l2switch.setChecked(true);
+                    }
+                }
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        f1EventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() != null) {
+                    int value = dataSnapshot.getValue(Integer.class);
+                    fanSeekBar.setProgress(value);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        l1Ref.addValueEventListener(l1EventListener);
+        l2Ref.addValueEventListener(l2EventListener);
+        f1Ref.addValueEventListener(f1EventListener);
     }
 
     private void removeListeners() {
@@ -205,6 +305,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         removeListeners();
+        removeDoorListener();
         super.onPause();
+    }
+
+    @Override
+    public void onResult(AIResponse result) {
+
+    }
+
+    @Override
+    public void onError(AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
     }
 }
